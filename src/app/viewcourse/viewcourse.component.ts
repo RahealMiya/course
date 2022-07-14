4d58fb8e9356368aaea1628868b7e6bb063fd269
RahealMiya <rahealmiya328@gmail.com>
2022-07-14 17:16:23 +0530

    service adding completed

--- src/app/viewcourse/viewcourse.component.ts
+++ src/app/viewcourse/viewcourse.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../api.service';
 
 @Component({
   selector: 'app-viewcourse',
@@ -7,9 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ViewcourseComponent implements OnInit {
 
-  constructor() { }
+  constructor(private api:ApiService) {
+    api.viewCourse().subscribe(
+      (r)=>{
+        this.data=r
+      }
+    )
+   }
 
   ngOnInit(): void {
   }
-
+data:any=[]
 }
